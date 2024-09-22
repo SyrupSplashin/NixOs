7daba45b706c388d0c8f11d6b4c1642ffccb30c0
SyrupSplashin <=>
2024-09-22 02:49:23 -0400

    Added initExtra to invoke p10k.zsh script

--- users/vertex/zsh.nix
+++ users/vertex/zsh.nix
@@ -24,5 +24,8 @@
         source "$\{XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-$\{(%):-%n}.zsh"
       fi
     '';
+    initExtra = ''
+      test -f ~/.config/zsh/.p10k.zsh && source ~/.config/zsh/.p10k.zsh
+    '';
   };
 }
