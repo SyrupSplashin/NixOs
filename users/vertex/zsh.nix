cdd4d0169fb27c95b0ad569d3f201326cb04ad1f
SyrupSplashin <=>
2024-09-22 02:32:25 -0400

    oops, forgot the oh-my-zsh bit

---
+++ users/vertex/zsh.nix
@@ -0,0 +1,17 @@
+{config, pkgs, ...}: {
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    autosuggestion.enable = true;
+    syntaxHighlightning.enable = true;
+    oh-my-zsh = {
+      enable = true;
+      theme = "powerlevel10k/powerlevel10k";
+    };
+    shellAliases = {
+      ls = "lsd";
+      vim = "nvim";
+      ip = "ip -c";
+    };
+  };
+}
