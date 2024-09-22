823da0be2955e354a505374eb408186862ca980d
SyrupSplashin <=>
2024-09-22 03:32:54 -0400

    troubleshooting p10k issues

--- users/vertex/zsh.nix
+++ users/vertex/zsh.nix
@@ -15,17 +15,11 @@
         src = pkgs.zsh-powerlevel10k;
         file = "share/zsh-powerlevel10k.zsh-theme";
       }
+      {
+        name = "powerlevel10k-config";
+        src = ./config;
+        file = "p10k.zsh";
+      }
     ];
-    initExtraFirst = ''
-      # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
-      # Initialization code that may require console input (password prompts, [y/n]
-      # confirmations, etc.) must go above this block; everything else may go below.
-      if [[ -r "$\{XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-$\{(%):-%n}.zsh" ]]; then
-        source "$\{XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-$\{(%):-%n}.zsh"
-      fi
-    '';
-    initExtra = ''
-      test -f ~/.config/zsh/.p10k.zsh && source ~/.config/zsh/.p10k.zsh
-    '';
   };
 }
