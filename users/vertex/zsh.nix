494ccee0f06345d6528d38678626f4c9b5229f90
SyrupSplashin <=>
2024-09-22 02:47:25 -0400

    reconfig zsh for powerlevel10k

--- users/vertex/zsh.nix
+++ users/vertex/zsh.nix
@@ -4,14 +4,25 @@
     enableCompletion = true;
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
-    oh-my-zsh = {
-      enable = true;
-      theme = "powerlevel10k/powerlevel10k";
-    };
     shellAliases = {
       ls = "lsd";
       vim = "nvim";
       ip = "ip -c";
     };
+    plugins = [
+      {
+        name = "powerlevel10k";
+        src = pkgs.zsh-powerlevel10k;
+        file = "share/zsh-powerlevel10k.zsh-theme";
+      }
+    ];
+    initExtraFirst = ''
+      # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+      # Initialization code that may require console input (password prompts, [y/n]
+      # confirmations, etc.) must go above this block; everything else may go below.
+      if [[ -r "$\{XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-$\{(%):-%n}.zsh" ]]; then
+        source "$\{XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-$\{(%):-%n}.zsh"
+      fi
+    '';
   };
 }
