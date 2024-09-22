568bc8dd2aa32316799b5e44c4b38e0ffb29066c
SyrupSplashin <vertex@syrupsplash.in>
2024-09-22 03:41:17 -0400

    latest changes

--- users/vertex/zsh.nix
+++ users/vertex/zsh.nix
@@ -13,7 +13,7 @@
       {
         name = "powerlevel10k";
         src = pkgs.zsh-powerlevel10k;
-        file = "share/zsh-powerlevel10k.zsh-theme";
+        file = "share/zsh-powerlevel10k/zsh-powerlevel10k.zsh-theme";
       }
       {
         name = "powerlevel10k-config";
