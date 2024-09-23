ed9fe9b8570c9bcfd5d6a7f9bb981378f7d568ef
SyrupSplashin <=>
2024-09-23 00:39:44 -0400

    Testing rofi extraConfig

--- vertex/packages/rofi/rofi.nix
+++ vertex/packages/rofi/rofi.nix
@@ -3,5 +3,17 @@
     package = pkgs.rofi-wayland;
     enable = true;
     theme = "${./squared-nord.rasi}";
+    extraConfig = {
+      case-sensitive = false;
+      show-icons = true;
+      drun-match-fields = "name,generic,exec,categories,keywords";
+      drun-display-format = "{name} [<span weight='light' size='small'><i>({generic})</i></span>]";
+      drun-show-actions = false;
+      drun = {
+        parse-user = true;
+        parse-system = true;
+      };
+      display-drun = "Apps";
+    };
   };
 }
