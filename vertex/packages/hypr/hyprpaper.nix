e42dc8adfb8285f41d30068acc48766baeac9e35
SyrupSplashin <vertex@syrupsplash.in>
2024-09-24 21:36:13 -0400

    Removing unused files

--- vertex/packages/hypr/hyprpaper.nix
+++
@@ -1,9 +0,0 @@
-{ config, pkgs, ...}: {
-  services.hyprpaper = {
-	enable = true;
-	settings = {
-		preload = "${./background/Background_Landscape.png}";
-		wallpaper = "eDP-1,${./background/Background_Landscape.png}";
-	};
-  };
-}
