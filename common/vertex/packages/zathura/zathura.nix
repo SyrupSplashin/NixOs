ab6a86fe0b931f9d60322f6367ae9bca454716ed
SyrupSplashin <vertex@syrupsplash.in>
2024-09-24 23:47:23 -0400

    modified zathura config

--- common/vertex/packages/zathura/zathura.nix
+++ common/vertex/packages/zathura/zathura.nix
@@ -1,8 +1,18 @@
 { config, pkgs, ...}: {
   programs.zathura = {
     enable = true;
+    extraConfig = ''
+    include ${./theme/catppuccin-mocha}
+    '';
     options = {
+      adjust-open = "best-fit";
+
+      scroll-page-aware = true;
+      smooth-scroll = true;
+      selection-clipboard = "wl-copy";
+
       recolor = true;
+      recolor-reverse-video = true;
       recolor-keephue = true;
     };
   };
