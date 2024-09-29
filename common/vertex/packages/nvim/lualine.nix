feba2631c480d2ec4700ac89032907834ed0954c
SyrupSplashin <vertex@syrupsplash.in>
2024-09-29 02:30:17 -0400

    added vim.opt.showmode = false due to presence of statusline

--- common/vertex/packages/nvim/lualine.nix
+++ common/vertex/packages/nvim/lualine.nix
@@ -1,17 +1,18 @@
 { config, pkgs, ...}: {
   programs.neovim = {
-  	plugins = with pkgs.vimPlugins; [
-	  lualine-nvim
-	];
-	extraLuaConfig = ''
+    plugins = with pkgs.vimPlugins; [
+      lualine-nvim
+    ];
+    extraLuaConfig = ''
       require("lualine").setup({
         options = {
-		  icons_enabled = true,
-		  theme = 'auto',
-		  section_separators = " ", 
-		  component_separators = " "
+          icons_enabled = true,
+          theme = 'auto',
+	  section_separators = " ", 
+	  component_separators = " "
         },
       })
+      vim.opt.showmode = false
     '';
   };
 }
