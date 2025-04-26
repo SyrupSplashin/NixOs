3a66a47647df220ad8f9d41e345166387886a88d
SyrupSplashin <vertex@syrupsplash.in>
2025-04-25 21:14:56 -0400

    Modified values to align with new changes to zsh pkg

--- common/vertex/packages/zsh/default.nix
+++ common/vertex/packages/zsh/default.nix
@@ -10,7 +10,7 @@
       ip = "ip -c";
       cat = "bat";
     };
-    initExtra = ''
+    initContent = ''
       zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
       zstyle ':completion:*' list-colors "''${(s.:.)LS_COLORS}"
       zstyle ':completion:*' menu select
