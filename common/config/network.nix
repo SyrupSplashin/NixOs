cbffb4409f36a567d916ba9f8f2530c4620fd96b
SyrupSplashin <vertex@syrupsplash.in>
2025-07-21 02:06:24 -0400

    Modified network configuration to allow inbound connections to stardew valley tcp/udp ports

--- common/config/network.nix
+++ common/config/network.nix
@@ -2,6 +2,13 @@
   networking.firewall = {
     enable = true;
     trustedInterfaces = [ "tailscale0" ];
-    allowedUDPPorts = [ 41641 ];
+    allowedUDPPorts = [
+      41641
+      24642
+    ];
+    allowedTCPPorts = [
+      41641
+      24642
+    ];
   };
 }
