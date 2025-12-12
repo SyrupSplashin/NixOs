5db3a883124a49c05f51904e6625b42c2edce5ad
SyrupSplashin <vertex@syrupsplash.in>
2025-12-12 15:31:46 -0500

    Forcing hm to overwrite mime list, resolves issue where builds would fail due to existing mimelist file

--- common/users/vertex/config/mime.nix
+++ common/users/vertex/config/mime.nix
@@ -1,12 +1,15 @@
 {
-  xdg.mimeApps = {
-    enable = true;
-    defaultApplications = {
-      "image/png" = [ "imv.desktop" ];
-      "image/jpeg" = [ "imv.desktop" ];
-      "image/gif" = [ "imv.desktop" ];
-      "image/svg+xml" = [ "imv.desktop" ];
-      "x-scheme-handler/obsidian" = [ "obsidian-url-handler.desktop" ];
+  xdg = {
+    configFile."mimeapps.list".force = true;
+    mimeApps = {
+      enable = true;
+      defaultApplications = {
+        "image/png" = [ "imv.desktop" ];
+        "image/jpeg" = [ "imv.desktop" ];
+        "image/gif" = [ "imv.desktop" ];
+        "image/svg+xml" = [ "imv.desktop" ];
+        "x-scheme-handler/obsidian" = [ "obsidian-url-handler.desktop" ];
+      };
     };
   };
 }
